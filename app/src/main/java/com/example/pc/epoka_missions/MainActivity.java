be51d9ec5fd1b2bce4f9f94a69a163f1d65365b8
GiveMeAPseudo <kevin.seggio@gmail.com>
2019-03-12 17:19:37 +0100

    Added Java Authentification

--- app/src/main/java/com/example/pc/epoka_missions/MainActivity.java
+++ app/src/main/java/com/example/pc/epoka_missions/MainActivity.java
@@ -2,8 +2,15 @@ package com.example.pc.epoka_missions;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends Activity {
 
@@ -13,12 +20,29 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
-    public void clic (View view) {
+    public void clic(View view) {
         EditText numero = (EditText) findViewById(R.id.etNumero);
         EditText password = (EditText) findViewById(R.id.etPassword);
+
 
         String urlServiceWeb = "http://172.16.47.15/epoka.php?numero=" + numero.getText() +
                 "&password=" + password.getText();
 
+
+        try {
+            if (numero.getText().toString() != "" && password.getText().toString() != "") {
+                InputStream is = null;
+                URL url = new URL(urlServiceWeb);
+                HttpURLConnection connexion = (HttpURLConnection) url.openConnection();
+                connexion.connect();
+                is = connexion.getInputStream();
+
+                BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            }
+        }
+        catch (Exception expt){
+            Log.e("log_tag", "Erreur pendant l'authentification" + expt.toString());
+
+        }
     }
 }
